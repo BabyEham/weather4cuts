929a74c9a39a09756ceab68e7bbd401ba059624b
서진교 <oygnijoes0209@gmail.com>
2025-11-19 17:04:45 +0900

    feat: add page and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-function App() {
+import GlobalToastContainer from "./components/Toast";
+import { BrowserRouter, Routes, Route } from "react-router";
+import MainPage from "./pages/MainPage";
+import CameraPage from "./pages/CameraPage";
+import ResultPage from "./pages/ResultPage";
 
+function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/camera" element={<CameraPage />} />
+          <Route path="/result" element={<ResultPage />} />
+        </Routes>
+      </BrowserRouter>
+      <GlobalToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
